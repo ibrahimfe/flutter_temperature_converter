91402094ad80bdadc9b78fb275ce5d06d05f937f
ibrahimfe <ibrahiimm.f@gmail.com>
2023-10-23 14:12:01 +0700

    changing the layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,98 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TemperatureConverterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TemperatureConverterApp extends StatefulWidget {
+  @override
+  _TemperatureConverterAppState createState() =>
+      _TemperatureConverterAppState();
+}
 
-  // This widget is the root of your application.
+class _TemperatureConverterAppState extends State<TemperatureConverterApp> {
+  double _inputValue = 0;
+  String _fromUnit = 'Celsius';
+  String _toUnit = 'Fahrenheit';
+  double _result = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Temperature Converter',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Temperature Converter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Convert from $_fromUnit to $_toUnit'),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _inputValue = double.tryParse(value) ?? 0;
+                    _convertTemperature();
+                  });
+                },
+              ),
+              DropdownButton<String>(
+                value: _fromUnit,
+                onChanged: (value) {
+                  setState(() {
+                    _fromUnit = value!;
+                    _convertTemperature();
+                  });
+                },
+                items: <String>['Celsius', 'Fahrenheit', 'Kelvin']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              Text('Convert to:'),
+              DropdownButton<String>(
+                value: _toUnit,
+                onChanged: (value) {
+                  setState(() {
+                    _toUnit = value!;
+                    _convertTemperature();
+                  });
+                },
+                items: <String>['Celsius', 'Fahrenheit', 'Kelvin']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              Text('Result: $_result $_toUnit'),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Temperature Converter'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  double inTemp = 0.0, outTemp = 0.0;
-  bool isFah = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Temperature Converter"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Enter Temperature',
-              labelText: isFah
-                  ? "You Entered $inTemp in Fahrenheit"
-                  : "You Entered $inTemp in Celcius",
-            ),
-            keyboardType: TextInputType.number,
-            onChanged: (newValue) {
-              setState(() {
-                try {
-                  inTemp = double.parse(newValue);
-                } catch (e) {
-                  //
-                }
-              });
-            },
-          ),
-          RadioListTile(
-            value: true,
-            groupValue: isFah,
-            onChanged: (newValue) {
-              setState(() {
-                isFah = newValue!;
-              });
-            },
-            title: Text("Fahrenheit"),
-          ),
-          RadioListTile(
-            value: false,
-            groupValue: isFah,
-            onChanged: (newValue) {
-              setState(() {
-                isFah = newValue!;
-              });
-            },
-            title: Text("Celcius"),
-          ),
-          ElevatedButton(
-            
-            onPressed: () {}, child: Text("Convert"))
-        ],
-      ),
-    );
+  void _convertTemperature() {
+    if (_fromUnit == 'Celsius' && _toUnit == 'Fahrenheit') {
+      _result = (_inputValue * 9 / 5) + 32;
+    } else if (_fromUnit == 'Fahrenheit' && _toUnit == 'Celsius') {
+      _result = (_inputValue - 32) * 5 / 9;
+    } else if (_fromUnit == 'Celsius' && _toUnit == 'Kelvin') {
+      _result = _inputValue + 273.15;
+    } else if (_fromUnit == 'Kelvin' && _toUnit == 'Celsius') {
+      _result = _inputValue - 273.15;
+    } else if (_fromUnit == 'Fahrenheit' && _toUnit == 'Kelvin') {
+      _result = (_inputValue - 32) * 5 / 9 + 273.15;
+    } else if (_fromUnit == 'Kelvin' && _toUnit == 'Fahrenheit') {
+      _result = (_inputValue - 273.15) * 9 / 5 + 32;
+    } else {
+      _result = _inputValue;
+    }
   }
 }
