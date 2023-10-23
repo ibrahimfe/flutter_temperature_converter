239063a8c54b47b5ef9dd582579b2d4813c256c6
ibrahimfe <ibrahiimm.f@gmail.com>
2023-10-23 18:50:35 +0700

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -23,55 +23,75 @@ class _TemperatureConverterAppState extends State<TemperatureConverterApp> {
         appBar: AppBar(
           title: Text('Temperature Converter'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Convert from $_fromUnit to $_toUnit'),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  setState(() {
-                    _inputValue = double.tryParse(value) ?? 0;
-                    _convertTemperature();
-                  });
-                },
-              ),
-              DropdownButton<String>(
-                value: _fromUnit,
-                onChanged: (value) {
-                  setState(() {
-                    _fromUnit = value!;
-                    _convertTemperature();
-                  });
-                },
-                items: <String>['Celsius', 'Fahrenheit', 'Kelvin']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              Text('Convert to:'),
-              DropdownButton<String>(
-                value: _toUnit,
-                onChanged: (value) {
-                  setState(() {
-                    _toUnit = value!;
-                    _convertTemperature();
-                  });
-                },
-                items: <String>['Celsius', 'Fahrenheit', 'Kelvin']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              Text('Result: $_result $_toUnit'),
-            ],
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Convert from $_fromUnit to $_toUnit',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Temperature',
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      _inputValue = double.tryParse(value) ?? 0;
+                      _convertTemperature();
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    DropdownButton<String>(
+                      value: _fromUnit,
+                      onChanged: (value) {
+                        setState(() {
+                          _fromUnit = value!;
+                          _convertTemperature();
+                        });
+                      },
+                      items: <String>['Celsius', 'Fahrenheit', 'Kelvin']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                    Icon(Icons.arrow_forward),
+                    DropdownButton<String>(
+                      value: _toUnit,
+                      onChanged: (value) {
+                        setState(() {
+                          _toUnit = value!;
+                          _convertTemperature();
+                        });
+                      },
+                      items: <String>['Celsius', 'Fahrenheit', 'Kelvin']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Result: $_result $_toUnit',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           ),
         ),
       ),
